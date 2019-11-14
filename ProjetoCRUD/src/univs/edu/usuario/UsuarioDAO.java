3c4781a4dd5ba235ad63faea9df759f07fd03342
Wéliton Vitor Alves <wellingtonvictoralves@gmail.com>
2019-11-14 21:01:28 -0200

    Cadastro Usuário

--- ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
+++ ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
@@ -2,6 +2,7 @@
 package univs.edu.usuario;
 
 import java.util.List;
+import javax.swing.JOptionPane;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
@@ -17,10 +18,10 @@ public class UsuarioDAO {
         transacao = sessao.beginTransaction();
         if(usuario.getIdUsuario() ==0){
         sessao.save(usuario);
-       
+       JOptionPane.showMessageDialog(null, "Usuario Cadastrado");
     }else{
         sessao.update(usuario);
-        
+        JOptionPane.showMessageDialog(null, "Usuario Editado");
         }
         transacao.commit();
         sessao.close();
